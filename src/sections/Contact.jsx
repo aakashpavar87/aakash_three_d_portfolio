ddcecc86487f6f9460457089f6a760e943ed2c3d
Aakash Pavar <aakashpavar87@gmail.com>
2024-11-24 21:35:46 +0530

    [refs:Portfolio][Added Contact And Project sections].

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,19 +1,47 @@
 import { useRef, useState } from "react"
+import { useMediaQuery } from "react-responsive";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
     const formRef = useRef()
     const [loading, setLoading] = useState(false)
+    const isMobile = useMediaQuery({ maxWidth: 768 });
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         message: ''
     })
-    const handleChange = () => {}
-    const handleSubmit = () => {}
+    const handleChange = ({target: {name, value}}) => {
+        setFormData({...formData, [name]: value})
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            await emailjs.send("service_1r4xisi", "template_hm7vklq", {
+                from_name: formData.name,
+                to_name: 'Aakash',
+                from_email: formData.email,
+                to_email: 'aakashpavar87@gmail.com',
+                message: formData.message
+            }, 'zZSbhs2fF5vPLExnP');
+            alert('Your Message has been sent !!!');
+        } catch (error) {
+            console.log('Error Has Occured While Sending Message...', error);
+            alert('Something went wrong !!!');
+        } finally {
+            setLoading(false)
+            setFormData({
+                name: '',
+                email: '',
+                message: ''
+            })
+        }
+    }
   return (
     <section className="c-space my-20">
         <div className="relative min-h-screen flex items-center justify-center flex-col">
-            <img src="/assets/terminal.png" alt="terminal background" className="absolute inset-0 min-h-screen" />
+            {!isMobile && <img src="/assets/terminal.png" alt="terminal background" className="absolute inset-0 min-h-screen" />}
             <div className="contact-container">
                 <h3 className="head-text">Let's Talk</h3>
                 <p className="text-lg text-white-600 mt-3">
@@ -62,7 +90,10 @@ const Contact = () => {
                             />
                         </span>
                     </label>
-                    
+                    <button disabled={loading} type="submit" className="field-btn">
+                        {loading ? "Sending ..." : "Send Message"}
+                        <img src="/assets/arrow-up.png" alt="arrow-up" className="field-btn_arrow" />
+                    </button>
                 </form>
             </div>
         </div>
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <section className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
+        <div className="text-white-500 flex gap-2">
+            <p>Tearms & Condition</p>
+            <p>|</p>
+            <p>Privacy Policy</p>
+        </div>
+        <div className="flex gap-5">
+            <div className="social-icon">
+                <img src="/assets/github.svg"  alt="github-icon" className="w-1/2 h-1/2" />
+            </div>
+            <div className="social-icon">
+                <img src="/assets/twitter.svg"  alt="twitter-icon" className="w-1/2 h-1/2" />
+            </div>
+            <div className="social-icon">
+                <img src="/assets/instagram.svg"  alt="instagram-icon" className="w-1/2 h-1/2" />
+            </div>
+        </div>
+        <p className="text-white-500">&copy; 2024 Aakash. All Rights Reserved.</p>
+    </section>
+  )
+}
+
+export default Footer
